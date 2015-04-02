71d0a0fd1d39fd05cdab0cdb162c4bb33d61d1aa
dmRoberto <roberto.d20@gmail.com>
2015-04-02 22:25:14 +0200

    Update clase. Se añade herencia con temaControllerEjb

--- jee_ecp/src/main/java/es/upm/miw/jee/ecp/controllers/ejb/RemoveTemaControllerEjb.java
+++ jee_ecp/src/main/java/es/upm/miw/jee/ecp/controllers/ejb/RemoveTemaControllerEjb.java
@@ -3,7 +3,8 @@ package es.upm.miw.jee.ecp.controllers.ejb;
 import es.upm.miw.jee.ecp.controllers.RemoveTemaController;
 import es.upm.miw.jee.ecp.models.daos.jpa.DaoJpaFactory;
 
-public class RemoveTemaControllerEjb implements RemoveTemaController{
+public class RemoveTemaControllerEjb extends TemaControllerEjb implements
+		RemoveTemaController {
 
 	public void removeTema(Integer temaId) {
 		DaoJpaFactory.getFactory().getTemaDao().deleteById(temaId);
