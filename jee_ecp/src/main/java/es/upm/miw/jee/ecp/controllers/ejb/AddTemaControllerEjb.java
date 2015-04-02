725d488514423ab2e40da4cbf0ad888e231b502e
dmRoberto <roberto.d20@gmail.com>
2015-04-02 22:24:55 +0200

    Update clase. Se añade herencia con temaControllerEjb

--- jee_ecp/src/main/java/es/upm/miw/jee/ecp/controllers/ejb/AddTemaControllerEjb.java
+++ jee_ecp/src/main/java/es/upm/miw/jee/ecp/controllers/ejb/AddTemaControllerEjb.java
@@ -5,7 +5,8 @@ import es.upm.miw.jee.ecp.models.daos.TemaDao;
 import es.upm.miw.jee.ecp.models.daos.jpa.DaoJpaFactory;
 import es.upm.miw.jee.ecp.models.entities.Tema;
 
-public class AddTemaControllerEjb implements AddTemaController {
+public class AddTemaControllerEjb extends TemaControllerEjb implements
+		AddTemaController {
 
 	public void addTema(Tema tema) {
 		TemaDao dao = DaoJpaFactory.getFactory().getTemaDao();
