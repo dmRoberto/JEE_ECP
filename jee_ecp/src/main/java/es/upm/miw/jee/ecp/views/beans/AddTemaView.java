ccae5f189e870a8f7cab66a603790fd579c3a2ea
dmRoberto <roberto.d20@gmail.com>
2015-04-02 19:59:13 +0200

    Add clase. Se añade el bean para la vista addTema

---
+++ jee_ecp/src/main/java/es/upm/miw/jee/ecp/views/beans/AddTemaView.java
@@ -0,0 +1,36 @@
+package es.upm.miw.jee.ecp.views.beans;
+
+import es.upm.miw.jee.ecp.controllers.AddTemaController;
+import es.upm.miw.jee.ecp.controllers.ejb.ControllerFactoryEjb;
+import es.upm.miw.jee.ecp.models.entities.Tema;
+
+public class AddTemaView {
+
+	private String errorMsg;
+
+	private Tema tema;
+
+	public AddTemaView() {
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public String process() {
+		AddTemaController controller = ControllerFactoryEjb.getFactory()
+				.getAddTemaController();
+		
+		controller.addTema(tema);
+		return "";
+	}
+
+}
