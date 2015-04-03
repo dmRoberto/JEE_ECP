9240191bc955267b194da3c008d8810c04f9da91
dmRoberto <roberto.d20@gmail.com>
2015-04-03 09:36:07 +0200

    Add clase. Se añade el controlador votoController

---
+++ jee_ecp/src/main/java/es/upm/miw/jee/ecp/controllers/VotoController.java
@@ -0,0 +1,11 @@
+package es.upm.miw.jee.ecp.controllers;
+
+import java.util.List;
+
+import es.upm.miw.jee.ecp.models.entities.Voto;
+
+public interface VotoController {
+
+	public List<Voto> getVotosList();
+
+}
