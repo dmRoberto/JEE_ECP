3b294a435f4d716fcbe53191801a621ff09d190c
dmRoberto <roberto.d20@gmail.com>
2015-04-03 19:25:33 +0200

    Update clase. Se implementan los metodos de removeTemaControllerWs

--- jee_ecp/src/main/java/es/upm/jee/ecp/controllers/ws/RemoveTemaControllerWs.java
+++ jee_ecp/src/main/java/es/upm/jee/ecp/controllers/ws/RemoveTemaControllerWs.java
@@ -2,6 +2,9 @@ package es.upm.jee.ecp.controllers.ws;
 
 import java.util.List;
 
+import javax.ws.rs.core.GenericType;
+
+import es.upm.jee.ecp.ws.TemaUris;
 import es.upm.miw.jee.ecp.controllers.RemoveTemaController;
 import es.upm.miw.jee.ecp.models.entities.Tema;
 
@@ -9,14 +12,18 @@ public class RemoveTemaControllerWs implements RemoveTemaController {
 
 	@Override
 	public Boolean removeTema(Integer temaId, String autorizacionCode) {
-		// TODO Auto-generated method stub
-		return null;
+		WsManager webServicesManager = ControllerWs.buildWebServiceManager(
+				TemaUris.PATH_TEMAS, String.valueOf(temaId));
+		webServicesManager.addParams("auth", autorizacionCode);
+		return webServicesManager.delete();
 	}
 
 	@Override
 	public List<Tema> getTemasList() {
-		// TODO Auto-generated method stub
-		return null;
+		GenericType<List<Tema>> gerericType = new GenericType<List<Tema>>() {
+		};
+		return ControllerWs.buildWebServiceManager(TemaUris.PATH_TEMAS)
+				.entities(gerericType);
 	}
 
 }
