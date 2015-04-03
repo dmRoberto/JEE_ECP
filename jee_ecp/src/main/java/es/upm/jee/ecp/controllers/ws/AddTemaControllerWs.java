e26be70b5910d6d51424ce08321652dfc1395955
dmRoberto <roberto.d20@gmail.com>
2015-04-03 18:49:55 +0200

    Update metodo. Se implementa el metodo addTema del controlador addTemaController

--- jee_ecp/src/main/java/es/upm/jee/ecp/controllers/ws/AddTemaControllerWs.java
+++ jee_ecp/src/main/java/es/upm/jee/ecp/controllers/ws/AddTemaControllerWs.java
@@ -1,5 +1,6 @@
 package es.upm.jee.ecp.controllers.ws;
 
+import es.upm.jee.ecp.ws.TemaUris;
 import es.upm.miw.jee.ecp.controllers.AddTemaController;
 import es.upm.miw.jee.ecp.models.entities.Tema;
 
@@ -7,8 +8,7 @@ public class AddTemaControllerWs implements AddTemaController {
 
 	@Override
 	public void addTema(Tema tema) {
-		// TODO Auto-generated method stub
-
+		ControllerWs.buildWebServiceManager(TemaUris.PATH_TEMAS).create(tema);
 	}
 
 }
