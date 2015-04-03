7ba707c38f3234812d6fca81eb97d7a8c6c1f4d2
dmRoberto <roberto.d20@gmail.com>
2015-04-03 09:36:51 +0200

    Update metodo. Se añade metodo de listado a la clase NivelEstudios

--- jee_ecp/src/main/java/es/upm/miw/jee/ecp/models/utils/NivelEstudios.java
+++ jee_ecp/src/main/java/es/upm/miw/jee/ecp/models/utils/NivelEstudios.java
@@ -1,5 +1,18 @@
 package es.upm.miw.jee.ecp.models.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum NivelEstudios {
-	SIN_ESTUDIOS, PRIMARIOS, SECUNDARIOS, SUPERIORES ;
+	SIN_ESTUDIOS, PRIMARIOS, SECUNDARIOS, SUPERIORES;
+
+	public static List<String> names() {
+
+		List<String> list = new ArrayList<String>();
+		for (NivelEstudios s : values()) {
+			list.add(s.name());
+		}
+
+		return list;
+	}
 }
