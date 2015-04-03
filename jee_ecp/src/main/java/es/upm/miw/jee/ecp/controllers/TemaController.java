9004f3190e16c93efa23fe63f8a82efcec9f6a2e
dmRoberto <roberto.d20@gmail.com>
2015-04-03 12:05:27 +0200

    Update clase. Se actualiza la el controlador temController con la funcionalidad de listar votos para un tema

--- jee_ecp/src/main/java/es/upm/miw/jee/ecp/controllers/TemaController.java
+++ jee_ecp/src/main/java/es/upm/miw/jee/ecp/controllers/TemaController.java
@@ -3,11 +3,14 @@ package es.upm.miw.jee.ecp.controllers;
 import java.util.List;
 
 import es.upm.miw.jee.ecp.models.entities.Tema;
+import es.upm.miw.jee.ecp.models.entities.Voto;
 
 public interface TemaController {
-	
+
 	public List<Tema> getTemasList();
-	
+
 	public Tema getTema(Integer id);
+
+	public List<Voto> getTemaVotos(Integer id);
 
 }
--- jee_ecp/src/main/java/es/upm/miw/jee/ecp/controllers/ejb/TemaControllerEjb.java
+++ jee_ecp/src/main/java/es/upm/miw/jee/ecp/controllers/ejb/TemaControllerEjb.java
@@ -5,6 +5,7 @@ import java.util.List;
 import es.upm.miw.jee.ecp.controllers.TemaController;
 import es.upm.miw.jee.ecp.models.daos.jpa.DaoJpaFactory;
 import es.upm.miw.jee.ecp.models.entities.Tema;
+import es.upm.miw.jee.ecp.models.entities.Voto;
 
 public class TemaControllerEjb implements TemaController {
 
@@ -16,6 +17,11 @@ public class TemaControllerEjb implements TemaController {
 	@Override
 	public Tema getTema(Integer id) {
 		return DaoJpaFactory.getFactory().getTemaDao().read(id);
+	}
+
+	@Override
+	public List<Voto> getTemaVotos(Integer id) {
+		return DaoJpaFactory.getFactory().getTemaDao().votosForTemaId(id);
 	}
 
 }
