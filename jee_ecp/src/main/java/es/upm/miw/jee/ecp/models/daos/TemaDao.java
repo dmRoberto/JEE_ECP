facb421bca283471e2f2fcf3734512db32580f64
dmRoberto <roberto.d20@gmail.com>
2015-04-03 12:08:42 +0200

    Update clase. Se actualiza la interfaz dao para Tema

--- jee_ecp/src/main/java/es/upm/miw/jee/ecp/models/daos/TemaDao.java
+++ jee_ecp/src/main/java/es/upm/miw/jee/ecp/models/daos/TemaDao.java
@@ -1,7 +1,12 @@
 package es.upm.miw.jee.ecp.models.daos;
 
+import java.util.List;
+
 import es.upm.miw.jee.ecp.models.entities.Tema;
+import es.upm.miw.jee.ecp.models.entities.Voto;
 
 public interface TemaDao extends GenericDao<Tema, Integer> {
+	
+	public List<Voto> votosForTemaId (Integer temaId);
 
 }
